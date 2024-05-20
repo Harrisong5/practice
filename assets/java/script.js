91029c483aae76c0d1783d49d421ec55b7a5e812
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-05-20 13:20:02 +0000

    accuracy roll for char1, stats added

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,27 +1,44 @@
 let char1 = {
-    name: "Oliver Twist",
-    health: 100,
+    name: "Player1",
+    health: 99,
+    stats: {
+      att: 99
+      str: 89
+      def: 99
+    },
     attacks: [
       {
-        name: "Eye Stab",
+        name: "Accurate",
         dialogue: "Here, hold this!",
       },
       {
-        name: "Whooping Cough",
+        name: "Aggressive",
         dialogue: "Catch this!",
       },
       {
-        name: "Stale Baguette",
+        name: "Defensive",
         dialogue: "Please sir, have some more... PAIN.",
       },
+      {
+        name: "Special Attack",
+        dialogue: "Please sir, have some more... PAIN.",
+      },
+      
     ],
     attack() {
-      let damage = Math.ceil(Math.random() * 10);
-      let attack = Math.floor(Math.random() * 3);
-      console.log(
-        `"${this.attacks[attack].dialogue}," shouted Oliver, as he used his ${this.attacks[attack].name} attack and dealt ${damage} damage.\n`,
-      );
-      return damage;
+      let accuracyroll = Math.floor(Math.random()*2*char1[this.stats.att])
+      if (accuracyroll >= char2[this.stats.def]){
+        let damage = Math.ceil(Math.random() * 9);
+        let attack = Math.floor(Math.random() * 4);
+        console.log(
+          `"${this.attacks[attack].dialogue}," shouted Oliver, as he used his ${this.attacks[attack].name} attack and dealt ${damage} damage.\n`,
+        );
+        return damage;
+      } else {
+        console.log('Player1 missed an attack.')
+        break
+      }
+      
     },
     die() {
       console.log(
@@ -31,25 +48,29 @@ let char1 = {
   };
   
   let char2 = {
-    name: "Hulk Jr",
-    health: 100,
+    name: "Player2",
+    health: 99,
     attacks: [
       {
-        name: "Hulk Smash",
+        name: "Accurate",
         dialogue: "HULK SMAAAAAAASH!!!!",
       },
       {
-        name: "Kerb Stomp",
+        name: "Agressive",
         dialogue: "HULK STOOOOOOMP!!!!",
       },
       {
-        name: "Gamma Burst",
+        name: "Defensive",
+        dialogue: "HULK BUUUUUUURRRRRST!!!!",
+      },
+      {
+        name: "Special attack",
         dialogue: "HULK BUUUUUUURRRRRST!!!!",
       },
     ],
     attack() {
       let damage = Math.ceil(Math.random() * 10);
-      let attack = Math.floor(Math.random() * 3);
+      let attack = Math.floor(Math.random() * 4);
       console.log(
         `"${this.attacks[attack].dialogue}," shouted Hulk, as he used his ${this.attacks[attack].name} attack and dealt ${damage} damage.\n`,
       );
